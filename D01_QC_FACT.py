604be62efee123df5680b28b870ae080a914631c
alexnickersonweather-sys <alexnickersonweather@gmail.com>
2026-09-07 09:33:17 -0400

    Update D01_QC_FACT.py

--- D01_QC_FACT.py
+++ D01_QC_FACT.py
@@ -566,7 +566,7 @@ for i, stat in stat_all.iterrows():
     s
     # save the dataset
     ncf = f_new + stat["Reg"] + ".nc"
-    ds.to_netcdf(path=ncf, mode="w")
+    ds.to_netcdf(path=ncf, mode="w", engine="netcdf4")
     
     e = time.time()
     print("Successfully saved " + str(stat["Reg"]) + 
