5dba6a66589b0e431c6941f8573e3d0dff25d037
alexnickersonweather-sys <alexnickersonweather@gmail.com>
2026-09-07 08:50:32 -0400

    Update A07_List_Download_NDBC.py
    Fixed bugs that were present in the original version.  This version now correctly runs on the developer's MacBook Pro M2.

--- A07_List_Download_NDBC.py
+++ A07_List_Download_NDBC.py
@@ -1,12 +1,17 @@
 # Purpose: Combine the lists into one
 #
 # Date of Version 01: May  23, 2025
+# Date of Version 02: Sep. 07, 2026
 #
 # Author 01: Alexander Nickerson
+# Author 02: Alexander Nickerson
+#
+# Version 02 debugs errors that were present in the first version.
 #
 
 import os
 import pandas as pd
+import numpy as np
     
 # open list of stations and remove the excess white space
 stat_all = pd.read_csv('Docs/List_Redux_NDBC.txt',
@@ -21,9 +26,21 @@ stat_all = stat_all.sort_values(by=['Org','Reg']).reset_index(drop=True)
 folder = "Data/NDBC/L00D/"
 file_all = sorted([f for f in os.listdir(folder)])
 
+# convert the list to a dataframe
+file_all = pd.DataFrame(file_all, columns=["Buoy"])
+
+# force convert the dataframe to a numeric dataframe so that Apple's "._" and
+#       "DS_Store" files get converted to NaN
+file_all["Buoy"] = pd.to_numeric(file_all["Buoy"], errors="coerce")
+
+# ID the NaN's, remove them from the data frame, and convert to integer
+good = ~np.isnan(file_all["Buoy"])
+file_all = file_all.loc[good,:].reset_index(drop=True)
+file_all["Buoy"] = file_all["Buoy"].astype("int64")
+
 # check for stations that actually downloaded data and keep those only
-is_down = stat_full.isin(file_all)
-stat_all = stat_all.loc[is_down == True]
+is_down = stat_all["Reg"].isin(file_all["Buoy"])
+stat_all = stat_all.loc[is_down == True].reset_index(drop=True)
 
 # prepare the columns and write them to a CSV file
 stat_all["Reg"] = stat_all["Reg"].astype(str).str.rjust(40) + " "
